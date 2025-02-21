2ebb223af4b274196b377cefc89a04b15b2cf7ae
Holy-Feast <shelofasstov@gmail.com>
2025-02-21 17:27:02 +0200

    test commit git connection

--- pioneerblocks.php
+++ pioneerblocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Pioneer blocks
  * Description:       Pioneer custom blocks.
- * Version:           0.1.2
+ * Version:           0.1.3
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            The WordPress Contributors
@@ -27,10 +27,7 @@ function create_block_pioneerblocks_block_init() {
 }
 add_action( 'init', 'create_block_pioneerblocks_block_init' );
 function enqueue_custom_block_assets() {
-	// Enqueue Bootstrap CSS
 	wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css', array(), '5.3.2');
-
-	// Enqueue Bootstrap JS (only if needed)
 	wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.2', true);
 }
 add_action('enqueue_block_assets', 'enqueue_custom_block_assets');
