541849c29d40c7a47abda49b9c31336bb6c6c895
wolf-hub <freelance@web-wolf.xyz>
2025-02-24 17:04:06 +0200

    updates2 commit

--- pioneerblocks.php
+++ pioneerblocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Pioneer blocks
  * Description:       Pioneer custom blocks.
- * Version:           0.1.3
+ * Version:           0.1.4
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            The WordPress Contributors
