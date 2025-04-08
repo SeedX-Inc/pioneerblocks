5feebdc08463a0bff670134385d326bbc68e12d2
Holy-Feast <shelofasstov@gmail.com>
2025-04-08 15:26:41 +0300

    update version

--- pioneerblocks.php
+++ pioneerblocks.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       Pioneer blocks
  * Description:       Pioneer custom blocks.
- * Version:           0.1.16
+ * Version:           0.1.18
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            The WordPress Contributors
