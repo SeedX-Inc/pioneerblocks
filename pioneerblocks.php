366b5865574f978802dc54ea436f9e406efa7b5c
Holy-Feast <shelofasstov@gmail.com>
2025-04-21 16:11:12 +0300

    blocks separation version

--- pioneerblocks.php
+++ pioneerblocks.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       Pioneer blocks
  * Description:       Pioneer custom blocks.
- * Version:           0.1.18
+ * Version:           0.1.19
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            The WordPress Contributors
